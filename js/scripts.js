b13a5e9ffba866fdb548b0e2c733a736f1ed80f3
Brian Borealis <brian.borealis@gmail.com>
2015-09-18 09:58:44 -0700

    pass 4th PizzaOrder test

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ function PizzaOrder(pizzaSize, pizzaQuantity, pizzaPrice) {
     this.pizzaPrice = pizzaPrice;
 }
 
+PizzaOrder.prototype.quantityPrice = function() {
+    var total = this.pizzaQuantity * this.pizzaPrice;
+    return total;
+
+}
+
 function PizzaTopping(toppingType, toppingPrice) {
     this.toppingType = toppingType;
     this.toppingPrice = toppingPrice;
--- specs/specs.js
+++ specs/specs.js
@@ -1,17 +1,22 @@
 describe('PizzaOrder', function() {
     it("creates a new order size", function() {
-        var testPizzaOrder = new PizzaOrder("large","1", 20);
+        var testPizzaOrder = new PizzaOrder("large",1, 20);
         expect(testPizzaOrder.pizzaSize).to.equal("large");
     });
 
     it("creates a new order quantity", function() {
-        var testPizzaOrder = new PizzaOrder("large","1", 20);
-        expect(testPizzaOrder.pizzaQuantity).to.equal("1")
+        var testPizzaOrder = new PizzaOrder("large",1, 20);
+        expect(testPizzaOrder.pizzaQuantity).to.equal(1)
     });
 
     it("creates a new pizza price", function() {
-        var testPizzaOrder = new PizzaOrder("large","1", 20);
+        var testPizzaOrder = new PizzaOrder("large",1, 20);
         expect(testPizzaOrder.pizzaPrice).to.equal(20);
+    });
+
+    it("calculates the total pizza price based on quantity and price", function() {
+        var testPizzaOrder = new PizzaOrder("large",2, 20);
+        expect(testPizzaOrder.quantityPrice()).to.equal(40);
     });
 
 });
