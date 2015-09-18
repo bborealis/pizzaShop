30eb2d337b140703f5c5691355a41984036eeeed
Brian Borealis <brian.borealis@gmail.com>
2015-09-18 09:31:47 -0700

    fail 1st pizzaTopping test

--- specs/specs.js
+++ specs/specs.js
@@ -7,6 +7,13 @@ describe('PizzaOrder', function() {
     it("creates a new order quantity", function() {
         var testPizzaOrder = new PizzaOrder("large","1");
         expect(testPizzaOrder.pizzaQuantity).to.equal("1")
-    })
+    });
 
+});
+
+describe("PizzaTopping", function() {
+    it("creates a new topping", function() {
+        var testPizzaTopping = new PizzaTopping("pineapple");
+        expect(testPizzaTopping.toppingType).to.equal("pineapple");
+    });
 });
