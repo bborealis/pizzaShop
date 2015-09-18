309cc4826857884a051e918c3614530d4ccc7492
Brian Borealis <brian.borealis@gmail.com>
2015-09-18 10:21:50 -0700

    update 3rd test and add sizePrice prototype. removed pizzaPrice from PizzaOrder constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-function PizzaOrder(pizzaSize, pizzaQuantity, pizzaPrice) {
+function PizzaOrder(pizzaSize, pizzaQuantity) {
     this.pizzaSize = pizzaSize;
     this.pizzaQuantity = pizzaQuantity;
-    this.pizzaPrice = pizzaPrice;
+}
+
+PizzaOrder.prototype.sizePrice = function() {
+    var smallPizza = "small";
+    var mediumPizza = "medium";
+    var largePizza = "large";
+    if (this.pizzaSize === smallPizza) {
+        return 10;
+    } else if (this.pizzaSize === mediumPizza) {
+        return 15;
+    } else {
+        return 20;
+    }
 }
 
 PizzaOrder.prototype.quantityPrice = function() {
-    var total = this.pizzaQuantity * this.pizzaPrice;
+    var total = this.pizzaQuantity * this.sizePrice(this.pizzaSize)
     return total;
-
 }
 
 function PizzaTopping(toppingType, toppingPrice) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,23 +1,24 @@
 describe('PizzaOrder', function() {
     it("creates a new order size", function() {
-        var testPizzaOrder = new PizzaOrder("large",1, 20);
+        var testPizzaOrder = new PizzaOrder("large",1);
         expect(testPizzaOrder.pizzaSize).to.equal("large");
     });
 
     it("creates a new order quantity", function() {
-        var testPizzaOrder = new PizzaOrder("large",1, 20);
+        var testPizzaOrder = new PizzaOrder("large",1);
         expect(testPizzaOrder.pizzaQuantity).to.equal(1)
     });
 
-    it("creates a new pizza price", function() {
-        var testPizzaOrder = new PizzaOrder("large",1, 20);
-        expect(testPizzaOrder.pizzaPrice).to.equal(20);
+    it("determines the correct price based on size", function() {
+        var testPizzaOrder = new PizzaOrder("small",1);
+        expect(testPizzaOrder.sizePrice()).to.equal(10);
     });
 
     it("calculates the total pizza price based on quantity and price", function() {
-        var testPizzaOrder = new PizzaOrder("large",2, 20);
+        var testPizzaOrder = new PizzaOrder("large",2);
         expect(testPizzaOrder.quantityPrice()).to.equal(40);
     });
+
 
 });
 
