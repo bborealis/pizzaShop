efb315c03d8f09401de71c931178e6e8a0471924
Brian Borealis <brian.borealis@gmail.com>
2015-09-18 09:42:40 -0700

    pass 3rd PizzaOrder test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function PizzaOrder(pizzaSize, pizzaQuantity) {
+function PizzaOrder(pizzaSize, pizzaQuantity, pizzaPrice) {
     this.pizzaSize = pizzaSize;
     this.pizzaQuantity = pizzaQuantity;
+    this.pizzaPrice = pizzaPrice;
 }
 
 function PizzaTopping(toppingType, toppingPrice) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,12 +1,17 @@
 describe('PizzaOrder', function() {
     it("creates a new order size", function() {
-        var testPizzaOrder = new PizzaOrder("large","1");
+        var testPizzaOrder = new PizzaOrder("large","1", 20);
         expect(testPizzaOrder.pizzaSize).to.equal("large");
     });
 
     it("creates a new order quantity", function() {
-        var testPizzaOrder = new PizzaOrder("large","1");
+        var testPizzaOrder = new PizzaOrder("large","1", 20);
         expect(testPizzaOrder.pizzaQuantity).to.equal("1")
+    });
+
+    it("creates a new pizza price", function() {
+        var testPizzaOrder = new PizzaOrder("large","1", 20);
+        expect(testPizzaOrder.pizzaPrice).to.equal(20);
     });
 
 });
